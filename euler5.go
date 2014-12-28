d13c803ab17cb30ed0f6b6cee91e04dd4fd5dc84
Max Jacobson <max@hardscrabble.net>
2014-12-28 10:38:22 -0500

    Small refactor of euler 5

--- euler5.go
+++ euler5.go
@@ -3,14 +3,14 @@ import "fmt"
 
 func main() {
   i := 1
-  for !coolNumber(i) {
+  for !coolNumber(i, 20) {
     i++
   }
   fmt.Println(i)
 }
 
-func coolNumber(num int) bool {
-  for i := 1; i <= 20; i++ {
+func coolNumber(num int, top int) bool {
+  for i:= top; i >= 1; i-- {
     if num % i != 0 {
       return false
     }
