71cae756edc028708cc191d23a2facccba3367b4
Max Jacobson <max@hardscrabble.net>
2014-12-28 09:25:13 -0500

    solve euler4

---
+++ euler4.go
@@ -0,0 +1,34 @@
+package main
+import(
+  "fmt"
+  "strconv"
+  "sort"
+  "strings"
+)
+
+func main() {
+  palindromes := []int{}
+
+  for i := 100; i <= 999; i++ {
+    for j := 100; j <= 999; j++ {
+      product := i * j
+      str := strconv.FormatInt(int64(product), 10)
+      if isPalindrome(str) {
+        palindromes = append(palindromes, product)
+      }
+    }
+  }
+
+  sort.Ints(palindromes)
+  fmt.Println(palindromes[ len(palindromes) - 1 ])
+}
+
+func isPalindrome(str string) bool {
+  chars := strings.Split(str, "")
+  for i := 0; i < len(chars); i++ {
+    if chars[i] != chars[ len(chars) - i - 1] {
+      return false
+    }
+  }
+  return true
+}
---
+++ strings.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+import "strings"
+
+func main() {
+  str := "hey"
+  fmt.Println(strings.Split(str, ""))
+}
+
