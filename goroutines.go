35803ce03a6e0534185933bd7d2358c60b2a3a5c
Max Jacobson <max@hardscrabble.net>
2015-01-06 22:15:44 -0500

    add goroutines example

---
+++ goroutines.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("started")
+  go process()
+  time.Sleep(time.Millisecond)
+  fmt.Println("done")
+}
+
+func process() {
+  fmt.Println("processing...")
+}
